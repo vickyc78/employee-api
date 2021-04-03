8f9b2d9650c12ce396403508185291a11bc4c749
vickywohlig <vicky.chotaliya@wohlig.com>
2021-04-03 23:52:28 +0530

    changes

--- models/index.js
+++ models/index.js
@@ -1,9 +1,20 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("employee", "abc", "abc", {
-  host: "localhost",
-  dialect: "postgres",
-  operatorsAliases: false
-});
+// const sequelize = new Sequelize("employee", "abc", "abc", {
+//   host: "localhost",
+//   dialect: "postgres",
+//   operatorsAliases: false
+// });
+
+const sequelize = new Sequelize(
+  "d88atsdo4jugpj",
+  "kfdhxrrompytoe",
+  "d4ee9d976313c89f4688188b3fb857e38b49b78d4013cff769a145b9647e9347",
+  {
+    host: "ec2-54-235-108-217.compute-1.amazonaws.com",
+    dialect: "postgres",
+    operatorsAliases: false
+  }
+);
 
 sequelize
   .authenticate()
